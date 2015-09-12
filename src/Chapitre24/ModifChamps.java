96cf9144b070d7458ec29bbd6ac366e8d8929498
Mibizina <andrainamihary@gmail.com>
2015-09-12 23:12:45 +0200

    chap24

---
+++ src/Chapitre24/ModifChamps.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapitre24;
+
+/**
+ *
+ * @author Mihary
+ */
+import java.lang.reflect.*;
+
+public class ModifChamps {
+    public static void main(String args[]) throws IllegalAccessException
+    {
+        Pointa p1 = new Pointa();
+        Pointa p2 = new Pointa();
+        Class<?> c = p1.getClass();
+        Field champs[] = c.getDeclaredFields();
+        // Recuperation des valeurs du premier champ de p1 et de p2
+        int xp1 = champs[0].    getInt(p1); // on suppose le type (int) connu
+        Object xp2 = champs[0]. get(p2); // on ne suppose pas de type particulier on obtiendra un objet de type Integer
+        System.out.println("type de xp2 = " + xp2.getClass().getName());
+        System.out.println("Pour p1, champ " + champs[0].getName() + " = " + xp1);
+        System.out.println("Pour p2, champ " + champs[1].getName() + " = " + xp2);
+        // Modification des valeurs du premier champ p1 et de p2
+        Integer io = 100;
+        champs[0].set(p2, io);
+        champs[1].setInt(p1, 999);
+        System.out.println("Pour p1, champ " + champs[0].getName() + " = " + champs[0].getInt(p1));
+        System.out.println("Pour p2, champ " + champs[1].getName() + " = " + champs[1].getDouble(p2));
+        System.out.print("appel affiche sur p1 - "); p1.affiche();
+        System.out.print("appel affiche sur p2 - "); p2.affiche();
+    }
+}
+class Pointa
+{
+        public Pointa(){x=0; y=0;}
+        public Pointa(int x, int y){this.x = x; this.y = y;}
+        void affiche() {System.out.println("coordonnees : " + x + " " + y);}
+public int x,y;
+}
